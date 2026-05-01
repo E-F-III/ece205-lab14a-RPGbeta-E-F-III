5af8f17dee2741d9a4a0122436af8120dfc66039
Menden Cannistra <mendenc@hawaii.edu>
2026-05-01 02:44:03 -1000

    Update AirBender.cpp

--- Characters/PlayerControlled/AirBender.cpp
+++ Characters/PlayerControlled/AirBender.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
-// University of Hawaii, College of Engineering
-// Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
+//  University of Hawaii, College of Engineering
+//  Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
 //
 /// @file    AirBender.cpp
 /// @author  Edward Felipe III <efelipe3@hawaii.edu>
@@ -9,23 +9,28 @@
 #include "PlayerCharacter.hpp"
 #include "AirBender.hpp"
 #include "Air_Bending.hpp"
+#include "util/TextDisplay.hpp"
+
 using namespace std;
+
 // --- Constructor and Destructor --- 
 /// Constructor: initializes the air bending system
 AirBender::AirBender(std::string& characterName, int& raceCode) : PlayerCharacter(characterName, raceCode) {
     airBendingSystem = new AirBending();
     // Initialize the air bending system pointer
 }
+
 // Destructor: This MUST be defined to clean up memory allocated in the constructor.
 AirBender::~AirBender() {
     delete airBendingSystem; 
 }
+
 /// use bending system to perform an air bending action (This function contains the menu logic)
 void AirBender::performAction(FighterCharacter& target) {
     int choice = -1;
     // Initialize choice variable for loop validation
-    cout << "\nGame Master: What would you like " << name << " to do?"
-         << endl;
+    util::printColor("\nGame Master: What would you like " + name + " to do?\n", util::FG_CYAN);
+    
     airBendingSystem->getAvailableBendingActions(*this); // Display available options
     
     // Loop ensured the options 0-2 are selected (Input Validation)
@@ -34,35 +39,38 @@ void AirBender::performAction(FighterCharacter& target) {
         cin >> choice;
         if (choice < 0 || choice > 2) {
             // Using endl for console consistency
-            cout << "Invalid input! Please enter a number between 0 and 2."
-                     << endl;
+            util::printColor("Invalid input! Please enter a number between 0 and 2.\n", util::FG_RED);
+            cout << endl;
         }
     }
     
     // Call the appropriate function in airBendingSystem based on user selection.
     switch (choice) {
         case 0:
-            cout << "\n--- Action Selected: Air Slice ---\n" << endl;
+            util::printColor("\n--- Action Selected: Air Slice ---\n", util::FG_YELLOW);
             airBendingSystem->airSlice(*this,target);
             // Passing target reference to modify health
             break;
         case 1:
-            cout << "\n--- Action Selected: Air Shield ---\n" << endl;
+            util::printColor("\n--- Action Selected: Air Shield ---\n", util::FG_YELLOW);
             airBendingSystem->airShield(*this, target); // Passing target reference to modify health
             break;
         case 2:
-            cout << "\n--- Action Selected: Flight ---\n" << endl;
+            util::printColor("\n--- Action Selected: Flight ---\n", util::FG_BLUE);
             airBendingSystem->flight(*this);
             break;
     }
 }
+
 /// Print AirBender-specific stats in addition to base stats
 void AirBender::printStats() {
     PlayerCharacter::printStats();
-    cout << "Profession: AirBender" << endl;
+    util::printColor("Profession: AirBender\n", util::FG_YELLOW);
     cout << "------------------------------------" << endl;
 }
+
 /// AirBender-specific greeting
 void AirBender::greet() const {
-    cout << name << " the AirBender: Greetings! I am " << name << ". The wind is at my command, and I will use it to protect my allies" << endl;
+    util::printColor(name + " the AirBender: ", util::FG_YELLOW);
+    util::printType("Greetings! I am " + name + ". The wind is at my command, and I will use it to protect my allies\n", 30);
 }
