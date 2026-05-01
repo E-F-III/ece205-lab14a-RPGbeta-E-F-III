311d4feb55644ca57c28f56be9ea79afecc517ce
Menden Cannistra <mendenc@hawaii.edu>
2026-05-01 02:45:32 -1000

    Update WaterBender.cpp

--- Characters/PlayerControlled/WaterBender.cpp
+++ Characters/PlayerControlled/WaterBender.cpp
@@ -1,16 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  University of Hawaii, College of Engineering
-//  Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
+// University of Hawaii, College of Engineering
+// Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
 //
 /// @file    WaterBender.cpp
 /// @author  Edward Felipe III <efelipe3@hawaii.edu>
 /// WaterBender is a derived class of PlayerCharacter.
 /////////////////////////////////////////////////////////////////////////////////
-
 #include <iostream>
 #include "PlayerCharacter.hpp"
 #include "WaterBender.hpp"
 #include "Water_Bending.hpp"
+#include "util/TextDisplay.hpp"
 
 using namespace std;
 
@@ -30,7 +30,8 @@ WaterBender::~WaterBender() {
 void WaterBender::performAction(FighterCharacter& target) {
     int choice = -1;
     // Initialize choice variable for loop validation
-    cout << "\nGame Master: What would you like " << name << " to do?" << endl;
+    util::printColor("\nGame Master: What would you like " + name + " to do?" << endl, util::FG_CYAN);
+    
     waterBendingSystem->getAvailableBendingActions(*this); // Display available actions
     
     // Loop ensures the options 0-2 are selected (Validation)
@@ -38,22 +39,23 @@ void WaterBender::performAction(FighterCharacter& target) {
         cout << "Enter the number corresponding to your choice (0=Healing, 1=Whip, 2=Barrier): ";
         cin >> choice;
         if (choice < 0 || choice > 2) {
-            cout << "Invalid input! Please enter a number between 0 and 2.\n" << endl;
+            util::printColor("Invalid input! Please enter a number between 0 and 2.\n", util::FG_RED);
+            cout << endl;
         }
     }
     
     // Call the appropriate function in waterBendingSystem based on user selection.
     switch (choice) {
         case 0:
-            cout << "\n--- Action Selected: Healing Waters ---\n" << endl;
+            util::printColor("\n--- Action Selected: Healing Waters ---\n", util::FG_CYAN);
             waterBendingSystem->healingWaters(*this, target);
             break;
         case 1:
-            cout << "\n--- Action Selected: Water Whip ---\n" << endl;
+            util::printColor("\n--- Action Selected: Water Whip ---\n", util::FG_CYAN);
             waterBendingSystem->waterWhip(*this, target);
             break;
         case 2:
-            cout << "\n--- Action Selected: Ice Barrier ---\n" << endl;
+            util::printColor("\n--- Action Selected: Ice Barrier ---\n", util::FG_CYAN);
             waterBendingSystem->iceBarrier(*this, target);
             break;
     }
@@ -62,11 +64,12 @@ void WaterBender::performAction(FighterCharacter& target) {
 /// Print WaterBender-specific stats in addition to base stats
 void WaterBender::printStats() {
     PlayerCharacter::printStats();
-    cout << "Profession: WaterBender" << endl;
+    util::printColor("Profession: WaterBender\n", util::FG_CYAN);
     cout << "------------------------------------" << endl;
 }
 
 /// WaterBender-specific greeting
 void WaterBender::greet() const {
-    cout << name << " the WaterBender: Greetings! I am " << name << ". The water is at my command, and I will use it to protect my allies" << endl;
+    util::printColor(name + " the WaterBender: ", util::FG_CYAN);
+    util::printType("Greetings! I am " + name + ". The water is at my command, and I will use it to protect my allies\n", 30);
 }
