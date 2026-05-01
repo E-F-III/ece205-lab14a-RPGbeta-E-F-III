1ec4c4266a54a1c2de9e587e0d26fbccbffe817c
Menden Cannistra <mendenc@hawaii.edu>
2026-05-01 02:42:56 -1000

    Update NPCFireBender.cpp

--- Characters/NPC/NPCFireBender.cpp
+++ Characters/NPC/NPCFireBender.cpp
@@ -1,18 +1,17 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  University of Hawaii, College of Engineering
-//  Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
+// University of Hawaii, College of Engineering
+// Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
 //
 /// @file    NPCFireBender.cpp
 /// @author  Edward Felipe III <efelipe3@hawaii.edu>
 /// NPCFireBender is a derived class of NPCharacter. FireBenders use fire manipulation to fight enemies and protect allies.
 /////////////////////////////////////////////////////////////////////////////////
-
 #include <iostream>
-
 #include "GameCharacter.hpp"
 #include "NPCharacter.hpp"
 #include "NPCFireBender.hpp"
 #include "Fire_Bending.hpp"
+#include "util/TextDisplay.hpp"
 
 using namespace std;
 
@@ -30,6 +29,8 @@ NPCFireBender::~NPCFireBender() {
 
 /// use bending system to perform a fire bending action
 void NPCFireBender::performAction(FighterCharacter& target) {
+    util::printColor("\n[AI] " + name + " performs a Fire Bending action!\n", util::FG_CYAN);
+    
     // for now, randomize the action selection for NPCs
     int choice = rand() % 3; // Randomly select an action (0, 1, or 2)
     switch (choice) {
@@ -51,11 +52,12 @@ void NPCFireBender::performAction(FighterCharacter& target) {
 /// Print FireBender-specific stats in addition to base stats
 void NPCFireBender::printStats() {
     NPCharacter::printStats();
-    cout << "Profession: FireBender" << endl;
+    util::printColor("Profession: FireBender\n", util::FG_RED);
     cout << "------------------------------------" << endl;
 }
 
 /// FireBender-specific greeting
 void NPCFireBender::greet() const {
-    cout << name << " the FireBender: Greetings! I am " << name << ". The flames are at my command, and I will use them to protect my allies" << endl;
+    util::printColor(name + " the FireBender: ", util::FG_RED);
+    util::printType("Greetings! I am " + name + ". The flames are at my command, and I will use them to protect my allies\n", 30);
 }
