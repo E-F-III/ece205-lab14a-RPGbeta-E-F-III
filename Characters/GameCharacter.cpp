92044056cffc912bc5aa7c7ec2006360f640bc58
Menden Cannistra <mendenc@hawaii.edu>
2026-05-01 18:16:17 -1000

    Update GameCharacter.cpp

--- Characters/GameCharacter.cpp
+++ Characters/GameCharacter.cpp
@@ -1,4 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
 // University of Hawaii, College of Engineering
 // Lab 11a - Game Character Class Part II - ECE 205 - Spring 2025
 //
@@ -12,30 +11,24 @@
 #include <string>
 #include "GameCharacter.hpp"
 #include "util/TextDisplay.hpp"
-
 //Default GameCharacter constructor
 GameCharacter::GameCharacter() = default;
-
 //create a game character object with a name
 GameCharacter::GameCharacter(std::string& newName){
     GameCharacter::name = newName;
 }
-
 //report values of object members
 std::string GameCharacter::getName(){
   return name;
 }
-
 //update values of object members
 void GameCharacter::setName(std::string& newName){
   name = newName;
 }
-
 void GameCharacter::greet() const{
   util::printColor(name + ": ", util::FG_WHITE);
   std::cout << "Hello, my name is " << name << "." << std::endl;
 }
-
 void GameCharacter::speak(std::string& response){
   util::printColor(name + ": ", util::FG_WHITE);
   std::cout << response << std::endl;
