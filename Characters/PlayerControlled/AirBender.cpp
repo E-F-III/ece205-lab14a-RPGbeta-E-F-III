cab76e37f9f68f72ea8971b9c367ff4f9700c69f
Menden Cannistra <mendenc@hawaii.edu>
2026-05-01 18:16:49 -1000

    Update AirBender.cpp

--- Characters/PlayerControlled/AirBender.cpp
+++ Characters/PlayerControlled/AirBender.cpp
@@ -1,5 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
-// University of Hawaii, College of Engineering
+//  University of Hawaii, College of Engineering
 // Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
 //
 /// @file    AirBender.cpp
@@ -10,28 +9,23 @@
 #include "AirBender.hpp"
 #include "Air_Bending.hpp"
 #include "util/TextDisplay.hpp"
-
 using namespace std;
-
 // --- Constructor and Destructor ---
 /// Constructor: initializes the air bending system
 AirBender::AirBender(std::string& characterName, int& raceCode) : PlayerCharacter(characterName, raceCode) {
     airBendingSystem = new AirBending();
     // Initialize the air bending system pointer
 }
-
 // Destructor: This MUST be defined to clean up memory allocated in the constructor.
 AirBender::~AirBender() {
     delete airBendingSystem;
 }
-
 /// use bending system to perform an air bending action (This function contains the menu logic)
 void AirBender::performAction(FighterCharacter& target) {
     int choice = -1;
     // Initialize choice variable for loop validation
     util::printColor("\nGame Master: What would you like " + name + " to do?\n", util::FG_CYAN);
     airBendingSystem->getAvailableBendingActions(*this); // Display available options
-
     // Loop ensured the options 0-2 are selected (Input Validation)
     while (choice < 0 || choice > 2) {
         cout << "Enter the number corresponding to your choice (0=Slice, 1=Shield, 2=Flight): ";
@@ -40,7 +34,6 @@ void AirBender::performAction(FighterCharacter& target) {
             cout << "Invalid input! Please enter a number between 0 and 2." << endl;
         }
     }
-
     // Call the appropriate function in airBendingSystem based on user selection.
     switch (choice) {
         case 0:
@@ -57,14 +50,12 @@ void AirBender::performAction(FighterCharacter& target) {
             break;
     }
 }
-
 /// Print AirBender-specific stats in addition to base stats
 void AirBender::printStats() {
     PlayerCharacter::printStats();
     util::printColor("Profession: AirBender\n", util::FG_YELLOW);
     cout << "------------------------------------" << endl;
 }
-
 /// AirBender-specific greeting
 void AirBender::greet() const {
     util::printColor(name + " the AirBender: ", util::FG_YELLOW);
