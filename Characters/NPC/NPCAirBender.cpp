1c18ead83a692420d386aa6c859acadf998868dc
Menden Cannistra <mendenc@hawaii.edu>
2026-05-01 18:18:11 -1000

    Update NPCAirBender.cpp

--- Characters/NPC/NPCAirBender.cpp
+++ Characters/NPC/NPCAirBender.cpp
@@ -1,6 +1,5 @@
-///////////////////////////////////////////////////////////////////////////////
 //  University of Hawaii, College of Engineering
-//  Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
+// Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
 //
 /// @file    NPCAirBender.cpp
 /// @author  Edward Felipe III <efelipe3@hawaii.edu>
@@ -12,21 +11,17 @@
 #include "NPCAirBender.hpp"
 #include "Air_Bending.hpp"
 #include "util/TextDisplay.hpp"
-
 using namespace std;
-
 // --- Constructor and Destructor --- 
 /// Constructor: initializes the air bending system
 NPCAirBender::NPCAirBender(std::string& characterName, int& raceCode) : NPCharacter(characterName, raceCode) {
     airBendingSystem = new AirBending();
     // Initialize the air bending system pointer
 }
-
 // Destructor: This MUST be defined to clean up memory allocated in the constructor.
 NPCAirBender::~NPCAirBender() {
     delete airBendingSystem; 
 }
-
 /// use bending system to perform an air bending action (This function contains the menu logic)
 void NPCAirBender::performAction(FighterCharacter& target) {
     util::printColor("\n[AI] " + name + " performs an Air Bending action!\n", util::FG_CYAN);
@@ -48,14 +43,12 @@ void NPCAirBender::performAction(FighterCharacter& target) {
             break;
     }
 }
-
 /// Print AirBender-specific stats in addition to base stats
 void NPCAirBender::printStats() {
     NPCharacter::printStats();
     util::printColor("Profession: AirBender\n", util::FG_YELLOW);
     cout << "------------------------------------" << endl;
 }
-
 /// AirBender-specific greeting
 void NPCAirBender::greet() const {
     util::printColor(name + " the AirBender: ", util::FG_YELLOW);
